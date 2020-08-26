53a12872937192207e014f5e6737cacbc63f2fd1
Axiuf <axiuff@163.com>
2020-08-26 13:04:45 +0800

    Create 3_1_GBM_total_merge_Normalization1_SCTransform.R

---
+++ src/3_1_GBM_total_merge_Normalization1_SCTransform.R
@@ -0,0 +1,26 @@
+# Seurat provides an improved method for normalization in the recent update "Seurat v3". Hence we will explore 
+# the different preprocessing in two seperate workflows.
+# Please see more information in satijalab.org/seurat/.
+# Here is the SCTransform workflow.
+
+
+# Set environment.
+source("requirements.R")
+
+
+# Get Seurat object.
+GBM <- readRDS("./data/2_GBM_total_merge_filtered.rds")
+
+
+# Calculate cell cycle score.
+GBM <- NormalizeData(GBM, normalization.method = "LogNormalize", scale.factor = 100000)
+
+load("./data/cycle.rda")
+GBM <- CellCycleScoring(GBM, g2m.features = g2m_genes, s.features = s_genes)
+
+
+# Using SCTransform in Seurat (replace "NormalizeData", "ScaleData", and "FindVariableFeatures")
+GBM <- SCTransform(GBM)
+
+
+saveRDS(GBM, file = "./data/3_1_GBM_total_merge_filtered_Normalization1_SCT.rds")
