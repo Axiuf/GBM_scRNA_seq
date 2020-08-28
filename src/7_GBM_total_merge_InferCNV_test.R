ab1bed8dfbd2eda25712e397048fa36b6c76dd8d
Axiuf <axiuff@163.com>
2020-08-28 11:11:38 +0800

    Update 7_GBM_total_merge_InferCNV_test.R

--- src/7_GBM_total_merge_InferCNV_test.R
+++ src/7_GBM_total_merge_InferCNV_test.R
@@ -36,13 +36,13 @@ counts_matrix <- GetAssayData(GBM, slot = "counts")
 
 # Get cell annotations.
 cell_Annotations <- GBM@meta.data["infercnv_type_tumor_patient"]
-write.table(cell_Annotations, file = paste0(subdir_path, '/cell_Annotations.txt'), quote = F, sep= "\t", col.names = FALSE)
+write.table(cell_Annotations, file = paste0(subdir_path, "/cell_Annotations.txt"), quote = F, sep= "\t", col.names = FALSE)
 
 
 # InferCNV
 infercnv_obj = CreateInfercnvObject(raw_counts_matrix = counts_matrix,
-                                    annotations_file = paste0(subdir_path, '/cell_Annotations.txt'),
-                                    gene_order_file = './data/gencode_v19_gen_pos.complete.txt',
+                                    annotations_file = paste0(subdir_path, "/cell_Annotations.txt"),
+                                    gene_order_file = "./data/gencode_v19_gen_pos.complete.txt",
                                     delim = "\t",
                                     ref_group_names = c("Macrophage / Microglia", "Oligodendrocyte", "T cell"))
 
